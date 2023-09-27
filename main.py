3c29acbe519a3e2aaabb8f7d2a685de6c1372145
alexanderclar30 <26911004-alexanderclar30@users.noreply.replit.com>
2023-09-27 11:51:36 +0000

    Simplifying Code

--- main.py
+++ main.py
@@ -11,41 +11,35 @@ while Comp_tally or User_tally <5:
     User_input = (input("""Rock, Paper or Scissors 
 >""")).lower()
 
-    Comp_choice = choice([1,2,3])
+    
+    Comp_choice = choice(Rock12)
 
-    if User_input == Rock12[0]:
-        Response = 3
-    elif User_input == Rock12[1]:
-        Response = 2
-    elif User_input == Rock12[2]:
-        Response = 1
-    elif User_input != Rock12:
+    if User_input != Rock12:
         print("that is not a valid input")
         Response = 0
 
-    if Response == Comp_choice:
+    if User_input == Comp_choice:
         print("its a draw")
         print(f"you both drew {User_input}")
 
-    if Response != Comp_choice:
-        if Response == 1 and Comp_choice == 2 :
-            print("You Win, the computer chose Paper")
-            User_tally += 1
-        if Response == 2 and Comp_choice == 3 :
-            print("You win, the computer chose Rock")
-            User_tally += 1
-        if Response == 3 and Comp_choice == 1 :
-            print("You win, the computer chose Scissors")
-            User_tally += 1
-        if Response == 1 and Comp_choice == 3 :
-            print("You lose, the computer chose Rock")
-            Comp_tally += 1
-        if Response == 2 and Comp_choice == 1 :
-            print("You lose, the computer chose Scissors")
-            Comp_tally += 1
-        if Response == 3 and Comp_choice == 2 :
-            print("You lose, the computer chose Paper")
-            Comp_tally += 1
+
+    if User_input != Comp_choice:
+      Loser = (print(f"You Lose!, computer chose {Comp_choice}"))
+      if User_input == Rock12[0]  and Comp_choice == Rock12[1] :
+        print(Loser)
+      elif User_input == Rock12[1] and Comp_choice == Rock12[2] :
+        print(Loser)
+      elif User_input == Rock12[2] and Comp_choice == Rock12[0] :
+        print(Loser)
+    
+      Comp_tally += 1
+    
+    else : 
+      print(f"You Win!, the computer chose{Comp_choice}")
+      User_tally += 1
+       
+    
+  
     print(f"""    SCORE
 Computer:{Comp_tally}
 Player:{User_tally}""")
